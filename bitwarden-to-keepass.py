cc5c6938c3bd993c58d72f5ea4ba731eb30e854a
lanjelin <6988319+Lanjelin@users.noreply.github.com>
2024-07-26 09:04:50 +0200

    Added PR from upstream https://github.com/davidnemec/bitwarden-to-keepass/pull/10

--- bitwarden-to-keepass.py
+++ bitwarden-to-keepass.py
@@ -3,101 +3,146 @@ import logging
 import os
 import re
 import subprocess
-
 from argparse import ArgumentParser
 from typing import Dict, List, Optional
 
 from pykeepass import PyKeePass, create_database
+from pykeepass.entry import Entry as KPEntry
 from pykeepass.exceptions import CredentialsError
 from pykeepass.group import Group as KPGroup
-from pykeepass.entry import Entry as KPEntry
 
 import folder as FolderType
-from item import Item, ItemType, CustomFieldType
+from item import CustomFieldType, Item, ItemType
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s :: %(levelname)s :: %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
+    format="%(asctime)s :: %(levelname)s :: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 kp: Optional[PyKeePass] = None
 
+
 def bitwarden_to_keepass(args):
     global kp
     try:
-        kp = PyKeePass(args.database_path, password=args.database_password, keyfile=args.database_keyfile)
+        kp = PyKeePass(
+            args.database_path,
+            password=args.database_password,
+            keyfile=args.database_keyfile,
+        )
     except FileNotFoundError:
-        logging.info('KeePass database does not exist, creating a new one.')
-        kp = create_database(args.database_path, password=args.database_password, keyfile=args.database_keyfile)
+        logging.info("KeePass database does not exist, creating a new one.")
+        kp = create_database(
+            args.database_path,
+            password=args.database_password,
+            keyfile=args.database_keyfile,
+        )
     except CredentialsError as e:
-        logging.error(f'Wrong password for KeePass database: {e}')
+        logging.error(f"Wrong password for KeePass database: {e}")
         return
 
-    folders = subprocess.check_output([args.bw_path, 'list', 'folders', '--session', args.bw_session], encoding='utf8')
+    folders = subprocess.check_output(
+        [args.bw_path, "list", "folders", "--session", args.bw_session], encoding="utf8"
+    )
     folders = json.loads(folders)
     groups_by_id = load_folders(folders)
-    logging.info(f'Folders done ({len(groups_by_id)}).')
+    logging.info(f"Folders done ({len(groups_by_id)}).")
 
-    items = subprocess.check_output([args.bw_path, 'list', 'items', '--session', args.bw_session], encoding='utf8')
+    items = subprocess.check_output(
+        [args.bw_path, "list", "items", "--session", args.bw_session], encoding="utf8"
+    )
     items = json.loads(items)
-    logging.info(f'Starting to process {len(items)} items.')
+    logging.info(f"Starting to process {len(items)} items.")
     for item in items:
-        if item['type'] in [ItemType.CARD, ItemType.IDENTITY]:
-            logging.warning(f'Skipping credit card or identity item "{item["name"]}".')
-            continue
+        # if item['type'] in [ItemType.CARD, ItemType.IDENTITY]:
+        #     logging.warning(f'Skipping credit card or identity item "{item["name"]}".')
+        #     continue
 
         bw_item = Item(item)
 
         is_duplicate_title = False
         try:
             while True:
-                entry_title = bw_item.get_name() if not is_duplicate_title else '{name} - ({item_id}'.format(name=bw_item.get_name(), item_id=bw_item.get_id())
+                entry_title = (
+                    bw_item.get_name()
+                    if not is_duplicate_title
+                    else "{name} - ({item_id}".format(
+                        name=bw_item.get_name(), item_id=bw_item.get_id()
+                    )
+                )
                 try:
                     entry = kp.add_entry(
                         destination_group=groups_by_id[bw_item.get_folder_id()],
                         title=entry_title,
                         username=bw_item.get_username(),
                         password=bw_item.get_password(),
-                        notes=bw_item.get_notes()
+                        notes=bw_item.get_notes(),
                     )
                     break
                 except Exception as e:
-                    if 'already exists' in str(e):
+                    if "already exists" in str(e):
                         is_duplicate_title = True
                         continue
                     raise
 
             totp_secret, totp_settings = bw_item.get_totp()
             if totp_secret and totp_settings:
-                entry.set_custom_property('TOTP Seed', totp_secret, protect=True)
-                entry.set_custom_property('TOTP Settings', totp_settings)
+                entry.set_custom_property("TOTP Seed", totp_secret, protect=True)
+                entry.set_custom_property("TOTP Settings", totp_settings)
 
-            uris = [uri['uri'] for uri in bw_item.get_uris()]
+            uris = [uri["uri"] for uri in bw_item.get_uris()]
             set_kp_entry_urls(entry, uris)
+
+            if item["type"] in [ItemType.CARD]:
+                card_items = bw_item.get_card_fields()
+                # for card_item in bw_item.get_card_fields():
+                # bw_item.get_card_fields()[card_item],
+                for card_item in card_items:
+                    entry.set_custom_property(card_item, card_items[card_item])
+
+            if item["type"] in [ItemType.IDENTITY]:
+                identity_items = bw_item.get_identity_fields()
+                # for identity_item in bw_item.get_identity_fields():
+                # identity_item, bw_item.get_identity_fields()[identity_item]
+                for identity_item in identity_items:
+                    entry.set_custom_property(
+                        identity_item, identity_items[identity_item]
+                    )
 
             for field in bw_item.get_custom_fields():
                 entry.set_custom_property(
-                    field['name'],
-                    field['value'],
-                    protect=field['type'] == CustomFieldType.HIDDEN,
+                    field["name"],
+                    field["value"],
+                    protect=field["type"] == CustomFieldType.HIDDEN,
                 )
 
             for attachment in bw_item.get_attachments():
-                attachment_raw = subprocess.check_output([
-                    args.bw_path, 'get', 'attachment', attachment['id'], '--raw', '--itemid', bw_item.get_id(),
-                    '--session', args.bw_session,
-                ])
+                attachment_raw = subprocess.check_output(
+                    [
+                        args.bw_path,
+                        "get",
+                        "attachment",
+                        attachment["id"],
+                        "--raw",
+                        "--itemid",
+                        bw_item.get_id(),
+                        "--session",
+                        args.bw_session,
+                    ]
+                )
                 attachment_id = kp.add_binary(attachment_raw)
-                entry.add_attachment(attachment_id, attachment['fileName'])
+                entry.add_attachment(attachment_id, attachment["fileName"])
 
         except Exception as e:
-            logging.warning(f'Skipping item named "{item["name"]}" because of this error: {repr(e)}')
+            logging.warning(
+                f'Skipping item named "{item["name"]}" because of this error: {repr(e)}'
+            )
             continue
 
-    logging.info('Saving changes to KeePass database.')
+    logging.info("Saving changes to KeePass database.")
     kp.save()
-    logging.info('Export completed.')
+    logging.info("Export completed.")
 
 
 def set_kp_entry_urls(entry: KPEntry, urls: List[str]) -> None:
@@ -107,18 +152,20 @@ def set_kp_entry_urls(entry: KPEntry, urls: List[str]) -> None:
     android_apps = ios_apps = extra_urls = 0
 
     for url in urls:
-        match url.partition('://'):
-            case ('androidapp', '://', app_id):
+        match url.partition("://"):
+            case ("androidapp", "://", app_id):
                 # It's an Android app registered by Bitwarden's mobile app
                 # Store multiple apps in AndroidApp, AndroidApp_1, etc. so that KeePassDX's autofill picks it up
-                prop_name = "AndroidApp" if android_apps == 0 else f"AndroidApp_{android_apps}"
+                prop_name = (
+                    "AndroidApp" if android_apps == 0 else f"AndroidApp_{android_apps}"
+                )
                 android_apps += 1
                 entry.set_custom_property(prop_name, app_id)
-            case ('iosapp', '://', app_id):
+            case ("iosapp", "://", app_id):
                 # It's an iOS app registered by Bitwarden's mobile app
                 # XXX Maybe properly set up autofill for a macOS/iPhone/iPad KeePass-compatible app like StrongBox or Keepassium
                 ios_apps += 1
-                entry.set_custom_property(f'iOS app #{ios_apps}', app_id)
+                entry.set_custom_property(f"iOS app #{ios_apps}", app_id)
             case _:
                 # Assume it's a generic URL.
                 # First one goes to the standard URL attribute and the remaining ones go to URL_1, URL_2 and so on
@@ -126,12 +173,12 @@ def set_kp_entry_urls(entry: KPEntry, urls: List[str]) -> None:
                     entry.url = url
                 else:
                     extra_urls += 1
-                    entry.set_custom_property(f'KP2A_URL_{extra_urls}', url)
+                    entry.set_custom_property(f"KP2A_URL_{extra_urls}", url)
 
 
 def load_folders(folders) -> Dict[str, KPGroup]:
     # sort folders so that in the case of nested folders, the parents would be guaranteed to show up before the children
-    folders.sort(key=lambda x: x['name'])
+    folders.sort(key=lambda x: x["name"])
 
     # dict to store mapping of Bitwarden folder id to keepass group
     groups_by_id: Dict[str, KPGroup] = {}
@@ -142,11 +189,15 @@ def load_folders(folders) -> Dict[str, KPGroup]:
     groups_by_id[None] = kp.root_group
 
     for folder in folders:
-        if folder['id'] is not None:
-            new_folder: FolderType.Folder = FolderType.Folder(folder['id'])
+        if folder["id"] is not None:
+            new_folder: FolderType.Folder = FolderType.Folder(folder["id"])
             # regex lifted from https://github.com/bitwarden/jslib/blob/ecdd08624f61ccff8128b7cb3241f39e664e1c7f/common/src/services/folder.service.ts#L108
-            folder_name_parts: List[str] = re.sub(r'^\/+|\/+$', '', folder['name']).split('/')
-            FolderType.nested_traverse_insert(folder_root, folder_name_parts, new_folder, '/')
+            folder_name_parts: List[str] = re.sub(
+                r"^\/+|\/+$", "", folder["name"]
+            ).split("/")
+            FolderType.nested_traverse_insert(
+                folder_root, folder_name_parts, new_folder, "/"
+            )
 
     # create keepass groups based off folder tree
     def add_keepass_group(folder: FolderType.Folder):
@@ -162,12 +213,16 @@ def load_folders(folders) -> Dict[str, KPGroup]:
 
 def check_args(args):
     if args.database_keyfile:
-        if not os.path.isfile(args.database_keyfile) or not os.access(args.database_keyfile, os.R_OK):
-            logging.error('Key File for KeePass database is not readable.')
+        if not os.path.isfile(args.database_keyfile) or not os.access(
+            args.database_keyfile, os.R_OK
+        ):
+            logging.error("Key File for KeePass database is not readable.")
             return False
 
     if not os.path.isfile(args.bw_path) or not os.access(args.bw_path, os.X_OK):
-        logging.error('bitwarden-cli was not found or not executable. Did you set correct \'--bw-path\'?')
+        logging.error(
+            "bitwarden-cli was not found or not executable. Did you set correct '--bw-path'?"
+        )
         return False
 
     return True
@@ -175,36 +230,35 @@ def check_args(args):
 
 def environ_or_required(key):
     return (
-        {'default': os.environ.get(key)} if os.environ.get(key)
-        else {'required': True}
+        {"default": os.environ.get(key)} if os.environ.get(key) else {"required": True}
     )
 
 
 parser = ArgumentParser()
 parser.add_argument(
-    '--bw-session',
-    help='Session generated from bitwarden-cli (bw login)',
-    **environ_or_required('BW_SESSION'),
+    "--bw-session",
+    help="Session generated from bitwarden-cli (bw login)",
+    **environ_or_required("BW_SESSION"),
 )
 parser.add_argument(
-    '--database-path',
-    help='Path to KeePass database. If database does not exists it will be created.',
-    **environ_or_required('DATABASE_PATH'),
+    "--database-path",
+    help="Path to KeePass database. If database does not exists it will be created.",
+    **environ_or_required("DATABASE_PATH"),
 )
 parser.add_argument(
-    '--database-password',
-    help='Password for KeePass database',
-    **environ_or_required('DATABASE_PASSWORD'),
+    "--database-password",
+    help="Password for KeePass database",
+    **environ_or_required("DATABASE_PASSWORD"),
 )
 parser.add_argument(
-    '--database-keyfile',
-    help='Path to Key File for KeePass database',
-    default=os.environ.get('DATABASE_KEYFILE', None),
+    "--database-keyfile",
+    help="Path to Key File for KeePass database",
+    default=os.environ.get("DATABASE_KEYFILE", None),
 )
 parser.add_argument(
-    '--bw-path',
-    help='Path for bw binary',
-    default=os.environ.get('BW_PATH', 'bw'),
+    "--bw-path",
+    help="Path for bw binary",
+    default=os.environ.get("BW_PATH", "bw"),
 )
 args = parser.parse_args()
 
--- item.py
+++ item.py
@@ -1,5 +1,5 @@
 from enum import IntEnum
-from urllib.parse import urlsplit, parse_qsl
+from urllib.parse import parse_qsl, urlsplit
 
 
 class ItemType(IntEnum):
@@ -8,76 +8,171 @@ class ItemType(IntEnum):
     CARD = 3
     IDENTITY = 4
 
+
 class CustomFieldType(IntEnum):
     TEXT = 0
     HIDDEN = 1
     BOOLEAN = 2
+
 
 class Item:
     def __init__(self, item):
         self.item = item
 
     def get_id(self) -> str:
-        return self.item['id']
+        return self.item["id"]
 
     def get_name(self) -> str:
-        return self.item['name']
+        return self.item["name"]
 
     def get_folder_id(self) -> str:
-        return self.item['folderId']
+        return self.item["folderId"]
 
     def get_username(self) -> str:
-        if 'login' not in self.item:
-            return ''
+        if "login" not in self.item:
+            return ""
 
-        return self.item['login']['username'] if self.item['login']['username'] else ''
+        return self.item["login"]["username"] if self.item["login"]["username"] else ""
 
     def get_password(self) -> str:
-        if 'login' not in self.item:
-            return ''
+        if "login" not in self.item:
+            return ""
 
-        return self.item['login']['password'] if self.item['login']['password'] else ''
+        return self.item["login"]["password"] if self.item["login"]["password"] else ""
 
     def get_notes(self):
-        return self.item['notes']
+        return self.item["notes"]
 
     def get_uris(self):
-        if 'login' not in self.item or 'uris' not in self.item['login']:
+        if "login" not in self.item or "uris" not in self.item["login"]:
             return []
 
-        for uri in self.item['login']['uris']:
-            uri['uri'] = uri['uri'] if uri['uri'] is not None else ''
+        for uri in self.item["login"]["uris"]:
+            uri["uri"] = uri["uri"] if uri["uri"] is not None else ""
 
-        return self.item['login']['uris']
+        return self.item["login"]["uris"]
+
+    def get_card_fields(self):
+        if "card" not in self.item:
+            return []
+
+        items = dict()
+        if self.item["card"]["cardholderName"] is not None:
+            items["Cardholder Name"] = self.item["card"]["cardholderName"]
+        if self.item["card"]["brand"] is not None:
+            items["Brand Name"] = self.item["card"]["brand"]
+        if self.item["card"]["number"] is not None:
+            items["Number"] = self.item["card"]["number"]
+        if self.item["card"]["expMonth"] is not None:
+            items["Expiration Month"] = self.item["card"]["expMonth"]
+        if self.item["card"]["expYear"] is not None:
+            items["Expiration Year"] = self.item["card"]["expYear"]
+        if self.item["card"]["code"] is not None:
+            items["Code"] = self.item["card"]["code"]
+
+        return items
+
+    def get_identity_fields(self):
+        if "identity" not in self.item:
+            return []
+
+        old_items = self.item["identity"]
+        items = dict()
+
+        temp = ""
+        found = False
+
+        # format Identity name as firstName middleName lastName
+        for i in ["title", "firstName", "middleName", "lastName"]:
+            if old_items[i] is not None:
+                if found:
+                    temp = " ".join([temp, old_items[i]])
+                else:
+                    found = True
+                    temp = "".join([temp, old_items[i]])
+        if temp != "":
+            items["Identity name"] = temp
+
+        if old_items["username"] is not None:
+            items["Identity username"] = old_items["username"]
+
+        if old_items["company"] is not None:
+            items["Company"] = old_items["company"]
+
+        if old_items["ssn"] is not None:
+            items["National Insurance number"] = old_items["ssn"]
+
+        if old_items["passportNumber"] is not None:
+            items["Passport number"] = old_items["passportNumber"]
+
+        if old_items["licenseNumber"] is not None:
+            items["License number"] = old_items["licenseNumber"]
+
+        if old_items["email"] is not None:
+            items["Email"] = old_items["email"]
+
+        if old_items["phone"] is not None:
+            items["Phone"] = old_items["phone"]
+
+        # form address string in below format
+        # address1
+        # address2
+        # address3
+        # city, state, postalCode
+        # country
+        temp, found = "", False
+        for i in ["address1", "address2", "address3"]:
+            if old_items[i] is not None:
+                if found:
+                    temp = "\n".join([temp, old_items[i]])
+                else:
+                    found = True
+                    temp = "".join([temp, old_items[i]])
+
+        found = False
+        for i in ["city", "state", "postalCode"]:
+            if old_items[i] is not None:
+                if found:
+                    temp = ", ".join([temp, old_items[i]])
+                else:
+                    found = True
+                    temp = "\n".join([temp, old_items[i]])
+
+        if old_items["country"] is not None:
+            temp = "\n".join([temp, old_items["country"]])
+        if temp != "":
+            items["Address"] = temp
+
+        return items
 
     def get_custom_fields(self):
-        if 'fields' not in self.item:
+        if "fields" not in self.item:
             return []
 
-        for field in self.item['fields']:
-            field['name'] = field['name'] if field['name'] is not None else ''
-            field['value'] = field['value'] if field['value'] is not None else ''
-            field['type'] = CustomFieldType(field['type'])
+        for field in self.item["fields"]:
+            field["name"] = field["name"] if field["name"] is not None else ""
+            field["value"] = field["value"] if field["value"] is not None else ""
+            field["type"] = CustomFieldType(field["type"])
 
-        return self.item['fields']
+        return self.item["fields"]
 
     def get_attachments(self):
-        if 'attachments' not in self.item:
+        if "attachments" not in self.item:
             return []
 
-        return self.item['attachments']
+        return self.item["attachments"]
 
     def get_totp(self):
-        if 'login' not in self.item:
+        if "login" not in self.item:
             return None, None
 
-        if not self.item['login']['totp']:
+        if not self.item["login"]["totp"]:
             return None, None
 
-        params = urlsplit(self.item['login']['totp']).query
+        params = urlsplit(self.item["login"]["totp"]).query
         params = dict(parse_qsl(params))
-        period = params.get('period', 30)
-        digits = params.get('digits', 6)
-        secret = params.get('secret', self.item['login']['totp'])
+        period = params.get("period", 30)
+        digits = params.get("digits", 6)
+        secret = params.get("secret", self.item["login"]["totp"])
 
-        return secret, f'{period};{digits}'
+        return secret, f"{period};{digits}"
