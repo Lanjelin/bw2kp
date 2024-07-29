563744ec6aa5a1eac44ae8bc0bd81007a678cbe2
lanjelin <6988319+Lanjelin@users.noreply.github.com>
2024-07-29 07:46:02 +0200

    Add support for /run/secrets in env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,38 @@
 #!/bin/env bash
-# Required
-[ -z "$DATABASE_PASSWORD" ] && echo "Need to set DATABASE_PASSWORD" && exit 1
-[ -z "$BW_CLIENTID" ] && echo "Need to set BW_CLIENTID" && exit 1
-[ -z "$BW_CLIENTSECRET" ] && echo "Need to set BW_CLIENTSECRET" && exit 1
-[ -z "$BW_PASSWORD" ] && echo "Need to set BW_PASSWORD" && exit 1
+# Required - var can be file, eg. /run/secrets
+if [ -z "$DATABASE_PASSWORD" ]; then
+	echo "Need to set DATABASE_PASSWORD"
+	exit 1
+else
+	if test -f "${DATABASE_PASSWORD}"; then
+		DATABASE_PASSWORD=$(<"${DATABASE_PASSWORD}")
+	fi
+fi
+if [ -z "$BW_CLIENTID" ]; then
+	echo "Need to set BW_CLIENTID"
+	exit 1
+else
+	if test -f "${BW_CLIENTID}"; then
+		BW_CLIENTID=$(<"${BW_CLIENTID}")
+	fi
+fi
+
+if [ -z "$BW_CLIENTSECRET" ]; then
+	echo "Need to set BW_CLIENTSECRET"
+	exit 1
+else
+	if test -f "${BW_CLIENTID}"; then
+		BW_CLIENTSECRET=$(<"${BW_CLIENTSECRET}")
+	fi
+fi
+if [ -z "$BW_PASSWORD" ]; then
+	echo "Need to set BW_PASSWORD"
+	exit 1
+else
+	if test -f "${BW_PASSWORD}"; then
+		BW_PASSWORD=$(<"${BW_PASSWORD}")
+	fi
+fi
 # Default values as fallback
 [ -z "$DATABASE_PATH" ] && export DATABASE_PATH='/exports/bitwarden-export.kdbx'
 [ -z "$BITWARDEN_URL" ] && export BITWARDEN_URL='https://bitwarden.com'
