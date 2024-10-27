54c80909d80e474b3aa8d7d2e1c20fc421224068
lanjelin <6988319+Lanjelin@users.noreply.github.com>
2024-10-27 18:18:49 +0100

    Backing up when writing to existing kdbx

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,38 +1,9 @@
 #!/bin/env bash
-# Required - var can be file, eg. /run/secrets
-if [ -z "$DATABASE_PASSWORD" ]; then
-	echo "Need to set DATABASE_PASSWORD"
-	exit 1
-else
-	if [ -f "$DATABASE_PASSWORD" ]; then
-		DATABASE_PASSWORD=$(<"$DATABASE_PASSWORD")
-	fi
-fi
-if [ -z "$BW_CLIENTID" ]; then
-	echo "Need to set BW_CLIENTID"
-	exit 1
-else
-	if [ -f "$BW_CLIENTID" ]; then
-		BW_CLIENTID=$(<"$BW_CLIENTID")
-	fi
-fi
-
-if [ -z "$BW_CLIENTSECRET" ]; then
-	echo "Need to set BW_CLIENTSECRET"
-	exit 1
-else
-	if [ -f "$BW_CLIENTSECRET" ]; then
-		BW_CLIENTSECRET=$(<"$BW_CLIENTSECRET")
-	fi
-fi
-if [ -z "$BW_PASSWORD" ]; then
-	echo "Need to set BW_PASSWORD"
-	exit 1
-else
-	if [ -f "$BW_PASSWORD" ]; then
-		BW_PASSWORD=$(<"$BW_PASSWORD")
-	fi
-fi
+# Required
+[ -z "$DATABASE_PASSWORD" ] && echo "Need to set DATABASE_PASSWORD" && exit 1
+[ -z "$BW_CLIENTID" ] && echo "Need to set BW_CLIENTID" && exit 1
+[ -z "$BW_CLIENTSECRET" ] && echo "Need to set BW_CLIENTSECRET" && exit 1
+[ -z "$BW_PASSWORD" ] && echo "Need to set BW_PASSWORD" && exit 1
 # Default values as fallback
 [ -z "$DATABASE_PATH" ] && export DATABASE_PATH='/exports/bitwarden-export.kdbx'
 [ -z "$BITWARDEN_URL" ] && export BITWARDEN_URL='https://bitwarden.com'
@@ -42,10 +13,15 @@ bw config server $BITWARDEN_URL >/dev/null
 bw login --apikey >/dev/null
 export BW_SESSION=$(bw unlock --passwordenv BW_PASSWORD | grep 'bw list items' | sed -e 's/.*session //') >/dev/null
 if bw sync >/dev/null; then
-	echo "Logged in and synced to Bitwarden."
+  echo "Logged in and synced to Bitwarden."
 else
-	echo "Unable to log in to Bitwarden."
-	exit 1
+  echo "Unable to log in to Bitwarden."
+  exit 1
+fi
+# Backup original file if exists
+if [ -f "$DATABASE_PATH" ]; then
+  echo "Made a backup of existing database."
+  cp "$DATABASE_PATH" "$DATABASE_PATH"_$(date -u +%Y-%m-%dT%H:%M:%S%Z).bak
 fi
 # Run the exporter
 python3 /bitwarden-to-keepass/bitwarden-to-keepass.py --bw-path /usr/bin/bw
